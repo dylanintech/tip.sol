47f5e06e5015fb318e9f516fd505a32dd85852c8
Dylan Molina <dylanivanmolina@gmail.com>
2022-07-13 10:24:51 -0500

    Adding the ability to post- not complete yet

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React, { useEffect, useState } from 'react';
+// import { Connection, PublicKey, clusterApiUrl } from '@solana/web3.js'
+// import { Program, Provider, web3 } from '@project-serum/anchor';
+// import kp from './keypair.json';
+// import idl from './idl.json';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faGithub, faDiscord, faTwitter } from '@fortawesome/free-brands-svg-icons'
@@ -8,10 +12,42 @@ import { BiUpvote } from "react-icons/bi";
 import { BiDownvote } from "react-icons/bi";
 import { IconContext } from "react-icons";
 library.add(faGithub, faDiscord, faTwitter);
+// const anchor = require('@project-serum/anchor');
+
+//utility function to shorten wallet address
+const shortenAddress = (str) => {
+  return str.substring(0, 6) + "..." + str.substring(str.length - 4);
+};
+
+//Solana runtime
+// const { SystemProgram } = web3;
+
+//Creating tip.sol account (which will hold our post data)
+// const arr = Object.values(kp._keypair.secretKey);
+// const secret = new Uint8Array(arr);
+// const baseAccount = web3.Keypair.fromSecretKey(secret);
+
+//Getting our program ID
+// const programID = new PublicKey(idl.metadata.address);
+
+//Setting network to devnet
+// const network = clusterApiUrl('devnet');
+
+//How we want to acknowledge when a transaction is done
+// const opts = {
+//   preflightCommitment: "processed",
+// }
 
 function App() {
-  //State
+  //Global State
   const [walletAddress, setWalletAddress] = useState(null);
+  const [creatingPost, setCreatingPost] = useState(false);
+  const [posts, setPosts] = useState([]);
+  //Post creation State
+  const [title, setTitle] = useState('');
+  const [intro, setIntro] = useState('');
+  const [body, setBody] = useState('');
+  const [conclusion, setConclusion] = useState('');
   //Function that holds logic for deciding if the Phantom Wallet is connected
   const checkIfWalletIsConnected = async () => {
     try {
@@ -47,6 +83,31 @@ function App() {
     }
   }
 
+  // const getProvider = () => {
+  //   const connection = new Connection(network, opts.preflightCommitment);
+  //   const provider = new Provider(
+  //     connection, window.solana, opts.preflightCommitment,
+  //   );
+  //   return provider;
+  // }
+
+  const addPost = () => {
+    //This is where you'll do the remote procedure call
+    const newPost = {
+      postTitle: title,
+      postIntro: intro,
+      postBody: body,
+      postConclusion: conclusion,
+    };
+    setPosts([...posts, newPost]);
+    setTitle('');
+    setIntro('');
+    setBody('');
+    setConclusion('');
+  }
+
+  //Render Functions
+
   //This UI is rendered when the user hasn't connected their wallet yet
   const renderNotConnectedContainer = () => {
     return (
@@ -61,11 +122,83 @@ function App() {
       </div>
     )
   }
-
+  
+  //This UI renders when a user has connected their wallet and is creating a new post
+  const renderPostForm = () => {
+    return (
+      <div>
+        <h1 className="py-2 font-bold text-xl ">Create your Post</h1>
+        <form 
+          onSubmit={(event) => {
+            event.preventDefault();
+            setCreatingPost(false);
+            addPost();
+            window.alert("You've successfully posted!");
+            // Call function that adds the post to the chain, when it's called remember to set every input value to an empty string
+          }}
+        >
+          <label for="title" className="mx-2">
+            Title
+          </label>
+          <input
+            type="text"
+            value={title}
+            placeholder="ex: 'Best places to learn about web3'"
+            name="title"
+            onChange={onTitleChange}
+            className="py-2 my-3 w-1/2 rounded-md"
+          />
+          <br />
+          <label for="intro" className="mx-2">
+            Intro
+          </label>
+          <input 
+            type="text" 
+            value={intro}
+            placeholder="ex: 'Everybody knows web3 is taking the world by storm...'"
+            name="intro"
+            onChange={onIntroChange}
+            className="py-2 my-3 w-1/2 rounded-md"
+          />
+          <br />
+          <label for="body" className="mx-2">
+            Body
+          </label>
+          <input
+            type="text"
+            value={body}
+            placeholder="What's the main idea of your post?"
+            name="body"
+            onChange={onBodyChange}
+            className="py-2 my-3 w-1/2 rounded-md"
+          />
+          <br />
+          <label for="conclusion" className="mx-2">
+            Outro
+          </label>
+          <input
+            type="text"
+            value={conclusion}
+            placeholder="ex: 'In the end, there are many great places to learn about web3...'"
+            name="conclusion"
+            onChange={onConclusionChange}
+            className="py-2 my-3 w-1/2 rounded-md"
+          />
+          <br />
+          <button className=" border-black border-2 rounded-lg w-1/3 bg-purple-500/50 hover:bg-purple-700/50 p-2 my-2" type="submit">Post!</button>
+        </form>
+      </div>
+    )
+  }
+  //This UI renders when the user has connected their wallet but is not creating a new post
   const renderConnectedContainer = () => {
     return (
       <div className="text-center text-white font-bold min-w-full pt-10">
         <h1 className="mb-6 text-8xl">Tip.sol</h1>
+        <button className="border-black border-2 rounded-lg w-1/3 bg-purple-500/50 shadow-2xl hover:bg-purple-700/50 hover:shadow-none p-2 my-2" onClick={(event) => {
+          event.preventDefault();
+          setCreatingPost(true)
+        }}>Create Post</button>
         {/* <p>Thanks for connecting! Tip.sol is a magical✨ place that lets you post useful content and get tipped for it. In Solana! Join our community👇 to be notified as features begin to roll out! We're glad to have you here.</p> */}
         <div className="flex flex-col items-center">
           <div className="w-1/2 border-black border-2 rounded-lg text-black bg-white p-4 my-4 shadow-xl">
@@ -83,7 +216,7 @@ function App() {
               </ol>
               <p className="py-2">I hope this post has helped you become more efficient and productive😁.</p>
               <div class="flex items-center justify-center">
-                <img className="py-2"src="https://media.giphy.com/media/pT4pmRFs15Yg8/giphy.gif"/>
+                <img className="py-2"src="https://media.giphy.com/media/pT4pmRFs15Yg8/giphy.gif" alt="productivity in action!"/>
               </div>
               <p className="py-2">**This is a preview of what posts on your feed will look like. Real posts won't have to be as long or as short but will generally follow a similar blog-post like format. Join our Discord if you're interested in using Tip.sol when it releases!**</p>
             </div>
@@ -111,6 +244,42 @@ function App() {
               </div>
             </div>
           </div>
+          {posts.map((post, index) => (
+           <div className="w-1/2 border-black border-2 rounded-lg text-black bg-white p-4 my-4 shadow-xl" key={index}>
+             <div className="block">
+               <h1 className="text-3xl ">{post.postTitle}</h1>
+               <p className="text-gray-500">Posted by B7KV...Elbj</p>
+             </div>
+             <div className="block">
+               <p className="py-2">{post.postIntro}</p>
+               <p className="py-2 font-normal">{post.postBody}</p>
+               <p className="py-2">{post.postConclusion}</p>
+             </div>
+             <div className="block border-t-2">
+               {/* <p className="float-left">Twitter: <a href="https://twitter.com/dxlantxch" target="_blank" rel="noreferrer"><span className="text-blue-700">@dxlantxch</span></a></p> */}
+               <div className="float-right flex">
+                 <IconContext.Provider value={{ style: { width: '3em', height: '2em'} }}>
+                   <div>
+                     <a href="" title="Add to favorites">
+                       <AiOutlineStar className=""/>
+                     </a>
+                   </div>
+                 </IconContext.Provider>
+                 <div className="pr-2">
+                   <a href="" title="This post was useful">
+                     <BiUpvote />
+                   </a>
+                   <a href="" title="This post was not useful">
+                     <BiDownvote />
+                   </a>
+                 </div>
+                 <a href="" title="Send SOL to B7KV...Elbj">
+                   <p className="pt-1 text-transparent bg-clip-text bg-gradient-to-br from-solana-green to-solana-purple">TIP</p>
+                 </a>
+               </div>
+             </div>
+           </div>
+          ))}
         </div>
       </div>
     )
@@ -125,17 +294,38 @@ function App() {
   const renderCommunity = () => {
     return (
       <div className="flex justify-evenly items-end text-center py-10">
-        <a href="https://github.com/Dmigit/tip.sol" target="_blank">
+        <a href="https://github.com/Dmigit/tip.sol" target="_blank" rel="noreferrer">
           <FontAwesomeIcon icon="fa-brands fa-github" size='3x' className="hover:h-14"/>
         </a>
-        <a href="https://discord.gg/UeZyQgBp" target="_blank">
+        <a href="https://discord.gg/UeZyQgBp" target="_blank" rel="noreferrer">
           <FontAwesomeIcon icon="fa-brands fa-discord" size='3x' className="hover:h-14" />
         </a>
-        <a href="https://twitter.com/Tipsolana" target="_blank">
+        <a href="https://twitter.com/Tipsolana" target="_blank" rel="noreferrer">
           <FontAwesomeIcon icon="fa-brands fa-twitter" size='3x' className="hover:h-14" />
         </a>
      </div>
     )
+  }
+  
+  //Input Change Handlers
+  const onTitleChange = (event) => {
+    const { value } = event.target;
+    setTitle(value);
+  }
+
+  const onIntroChange = (event) => {
+    const { value } = event.target;
+    setIntro(value);
+  }
+
+  const onBodyChange = (event) => {
+    const { value } = event.target;
+    setBody(value);
+  }
+
+  const onConclusionChange = (event) => {
+    const { value } = event.target;
+    setConclusion(value);
   }
 
   //When the component mounts for the first time, check to see if the Phantom wallet is connected
@@ -146,13 +336,15 @@ function App() {
     window.addEventListener('load', onLoad);
     return () => window.removeEventListener('load', onLoad);
   }, []);
-
+  
+  //App renders
   return (
-    <div className={`text-center from-solana-purple to-solana-green bg-gradient-to-r w-full ${walletAddress ? 'h-full lg:h-full' : 'h-screen' }`}>
-      {!walletAddress && renderTitle()}
-      {!walletAddress && renderNotConnectedContainer()}
-      {walletAddress && renderConnectedContainer()}
-      {walletAddress && renderCommunity()}
+    <div className={`text-center from-solana-purple to-solana-green bg-gradient-to-r w-full ${creatingPost ? 'h-screen' : walletAddress ? 'h-full lg:h-full' : 'h-screen' }`}>
+      {!walletAddress && !creatingPost && renderTitle()}
+      {!walletAddress && !creatingPost && renderNotConnectedContainer()}
+      {walletAddress && !creatingPost && renderConnectedContainer()}
+      {walletAddress && !creatingPost && renderCommunity()}
+      {walletAddress && creatingPost && renderPostForm()}
     </div>
   );
 }
